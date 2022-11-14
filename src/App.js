e5e9e0e70d20fb878a180b76a59d1aa455f0f46e
Shivanigitchouhan <109156762+Shivanigitchouhan@users.noreply.github.com>
2022-11-14 22:52:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Descriptions from "./Components/Descriptions";
 import { getFormattedWeatherData } from "./weatherService";
 
 function App() {
-  const [city, setCity] = useState("Bhilai");
+  const [city, setCity] = useState("Indore");
 
   const [weather, setWeather] = useState(null);
 
